d6563b79cf9f31b44635437429fc6df613fcc932
Charpell <charpellumeh@gmail.com>
2018-01-01 06:05:56 +0100

    feat(signup-api): Implement the SIgn up API endpoint - The api ensures the user signs up and gets a jwt token [Delivers #153284217]

--- server/config/mongooseSetting.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const mongooseSetting = async () =>
-  mongoose.connect(process.env.DATABASE_URL);
-
-export default mongooseSetting;
